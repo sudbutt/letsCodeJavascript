b2fd83868cc1ced4ab79f07539eb4d20d02423a2
sudbutt <sudbutt@hotmail.com>
2013-11-13 14:29:50 -0600

    added integrate step

--- Jakefile.js
+++ Jakefile.js
@@ -16,6 +16,21 @@ task("lint", function(){
     lint.validateFileList(files, lintOptions(), {});
 });
 
+desc("Integration checklist");
+task("integrate", ["default"], function() {
+        console.log("1. Make sure 'git status' is clean.");
+        console.log("2. Build on the integration box.");
+        console.log("   a. Walk over to integration box.");
+        console.log("   b. 'git pull'");
+        console.log("   c. 'npm rebuild'");
+        console.log("   d. Check status for files that need to be .gitignore'd");
+        console.log("   e. 'jake'");
+        console.log("   f. If jake fails, stop! Try again after fixing the issue.");
+        console.log("3. 'git checkout integration'");
+        console.log("4. 'git merge master --no-ff --log'");
+        console.log("5. 'git checkout master'");
+});
+
 desc("run unit tests");
 task("test", function() {
     var reporter = require('nodeunit').reporters.default;
