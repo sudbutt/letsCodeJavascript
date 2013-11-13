24320882462a28e8e22e70ab892a28fdea633ca5
sudbutt <sudbutt@hotmail.com>
2013-11-12 22:11:52 -0600

    adding linting of all js files

--- Jakefile.js
+++ Jakefile.js
@@ -2,8 +2,16 @@ desc("Default");
 task("default", ["lint"], function(){
 });
 
+
 desc("Lint everything");
 task("lint", function(){
-	var lint = require("./build/lint/lint_runner.js");
-	lint.validateFile("Jakefile.js", {}, {});
+    var lint = require("./build/lint/lint_runner.js");
+    var files = new jake.FileList();
+    var options = {
+        node: true
+    };
+    
+    files.include("**/*.js");
+    files.exclude("node_modules");
+    lint.validateFileList(files, options, {});
 });
